9e1bf8afce6328f7a48fb38f5d642dce86d24ed0
Sean Oxspring <Sean.Oxspring@hotmail.com>
2012-10-01 22:12:27 +0100

    Improved Prototype
    Added basic 'driving' abilities and some visual effects just to make it
    look nicer.

--- Prototype/GameDesignProject/Assets/CarControl.cs
+++ Prototype/GameDesignProject/Assets/CarControl.cs
@@ -18,5 +18,25 @@ public class CarControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	controller.AddForce(new Vector3(CarSpeed,0,0));
+		
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+		controller.AddForce(new Vector3(0,0,-100));
+		}
+		
+	
+		if (Input.GetKey (KeyCode.LeftArrow))
+		{
+		controller.AddForce(new Vector3(0,0,100));
+		}
+		
+		if (this.transform.position.x >= 103)
+		{
+		this.transform.position = new Vector3(-101,this.transform.position.y,this.transform.position.z);
+		this.CarSpeed += 10;
+		}
+		
 	}
+	
+	
 }
