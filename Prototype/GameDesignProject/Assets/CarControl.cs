bf83b85968bc84e3fed7b809cc2b9eb834c38ef0
Sean Oxspring <Sean.Oxspring@hotmail.com>
2012-10-07 14:20:47 +0100

    General Update

--- Prototype/GameDesignProject/Assets/CarControl.cs
+++ Prototype/GameDesignProject/Assets/CarControl.cs
@@ -9,25 +9,93 @@ public class CarControl : MonoBehaviour {
 	
 	public float CarSpeed;
 	
+	static int laneState;
+	//1 = Left
+	//2 = Middle
+	//3 = Right
+	
+	static int sensing;
+	
+	static Vector3 moveToLocation;
+	static bool buttonPressed;
+	
 	// Use this for initialization
 	void Start () {
 	controller = GetComponent<Rigidbody>();
-	
+	laneState = 2;
+	moveToLocation = controller.position;
+		sensing = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	controller.AddForce(new Vector3(CarSpeed,0,0));
+	controller.AddForce(new Vector3(CarSpeed,-100,0));
 		
-		if (Input.GetKey (KeyCode.RightArrow))
+		if (buttonPressed == true)
 		{
-		controller.AddForce(new Vector3(0,0,-100));
+		sensing++;
 		}
 		
-	
-		if (Input.GetKey (KeyCode.LeftArrow))
+		if (sensing == 10)
 		{
-		controller.AddForce(new Vector3(0,0,100));
+		buttonPressed = false;
+			sensing = 0;
+		}
+			
+		if(buttonPressed == false)
+		{
+			sensing = 0;
+		if(laneState == 1) //left
+		{
+			Debug.Log ("Left");
+			moveToLocation = new Vector3(moveToLocation.x,moveToLocation.y,2.5f);
+		
+			if (Input.GetKey (KeyCode.RightArrow))
+			{
+					buttonPressed = true;
+				laneState = 2;
+			}
+
+		}
+		else if(laneState == 2) //middle
+		{
+			Debug.Log ("Middle");
+			moveToLocation = new Vector3(moveToLocation.x,moveToLocation.y,0);
+			
+						if (Input.GetKey (KeyCode.RightArrow))
+			{
+					buttonPressed = true;
+				laneState = 3;
+			}
+			
+						if (Input.GetKey (KeyCode.LeftArrow))
+			{
+					buttonPressed = true;
+				laneState = 1;
+			}
+			
+		}
+		else if(laneState == 3) //right
+		{
+			Debug.Log ("Right");
+			moveToLocation = new Vector3(moveToLocation.x,moveToLocation.y,-2.5f);
+		
+			if (Input.GetKey (KeyCode.LeftArrow))
+			{
+					buttonPressed = true;
+				laneState = 2;
+			}
+		}
+		}
+		
+		if (controller.position.z < moveToLocation.z)	
+		{
+			controller.AddForce(new Vector3(0,0,500));
+		}
+		
+		if (controller.position.z > moveToLocation.z)	
+		{
+			controller.AddForce(new Vector3(0,0,-500));
 		}
 		
 		if (this.transform.position.x >= 103)
